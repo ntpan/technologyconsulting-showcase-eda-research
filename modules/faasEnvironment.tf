a26765eeb8af1d40780a4f3051b917deac015029
Tobias Bohn <tobiasbohnxz@gmail.com>
2020-11-17 12:31:15 +0100

    Customer Consumer Function Environment

--- modules/faasEnvironment.tf
+++ modules/faasEnvironment.tf
@@ -2,6 +2,7 @@ resource "azurerm_app_service_plan" "function_app_service_plan" {
     name = "tc-eda-iac-${var.environment}-function-app-service-plan"
     location = azurerm_resource_group.resourceGroup.location
     resource_group_name = azurerm_resource_group.resourceGroup.name
+    kind = "Windows"
 
     sku {
         tier = "Free"
@@ -9,8 +10,22 @@ resource "azurerm_app_service_plan" "function_app_service_plan" {
     }
 }
 
-resource "azurerm_function_app" "customer_function" {
-    name = "tc-eda-iac-${var.environment}-function-customer"
+resource "azurerm_function_app" "customer_function_producer" {
+    name = "tc-eda-iac-${var.environment}-function-customer-producer"
+    location = azurerm_resource_group.resourceGroup.location
+    resource_group_name = azurerm_resource_group.resourceGroup.name
+    app_service_plan_id = azurerm_app_service_plan.function_app_service_plan.id
+    storage_account_name = azurerm_storage_account.storage_account.name
+    storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
+    version = "~3"
+
+    identity {
+        type = "SystemAssigned"
+    }
+}
+
+resource "azurerm_function_app" "customer_function_consumer" {
+    name = "tc-eda-iac-${var.environment}-function-customer-consumer"
     location = azurerm_resource_group.resourceGroup.location
     resource_group_name = azurerm_resource_group.resourceGroup.name
     app_service_plan_id = azurerm_app_service_plan.function_app_service_plan.id
