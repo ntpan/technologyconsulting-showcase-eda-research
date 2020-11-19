15e1f3070d5d68341068b9bbc6fab79fbafbd329
Tobias Bohn <tobiasbohnxz@gmail.com>
2020-11-19 09:56:41 +0100

    Minor fix to keyvault access policy

--- modules/keyVault.tf
+++ modules/keyVault.tf
@@ -5,25 +5,23 @@ resource "azurerm_key_vault" "key_vault" {
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "standard"
   enabled_for_disk_encryption = true
-}
 
-#Terraform for secret creation
-resource "azurerm_key_vault_access_policy" "key_vault_terraform_access_policy" {
-    key_vault_id = azurerm_key_vault.key_vault.id
+  #Terraform for secret creation
+  access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
 
     secret_permissions = [ "delete", "get", "set", "list" ]
   }
-
-# PA TC
-resource "azurerm_key_vault_access_policy" "key_vault_pa_tc_access_policy" {
-  key_vault_id = azurerm_key_vault.key_vault.id
+  # PA TC
+  access_policy {
   tenant_id = data.azurerm_client_config.current.tenant_id
   object_id = "3f17a18b-59fc-4c13-a6d2-5852c4d8312a"
 
   secret_permissions = ["delete", "get", "set", "list"]
+  }
 }
+
 
 # Producer FaaS 
 resource "azurerm_key_vault_access_policy" "key_vault_producer_function_access_policy" {
