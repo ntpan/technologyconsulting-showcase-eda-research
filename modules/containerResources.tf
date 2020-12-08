102ae4c0f375ee35f812083b1c5abef040b041a2
Tobias Bohn <tobiasbohnxz@gmail.com>
2020-12-08 10:30:55 +0100

    remove unneeded container registry

--- modules/containerResources.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_container_registry" "container_registry" {
-    name = "TcEdaIac${var.environment}ContainerRegistry"
-    location = azurerm_resource_group.resourceGroup.location
-    resource_group_name = azurerm_resource_group.resourceGroup.name
-    admin_enabled = true
-    sku = "Standard"
-}
